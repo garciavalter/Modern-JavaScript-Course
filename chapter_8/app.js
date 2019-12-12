ead490068b79520fe50897f385fd1eeb875e6358
Valter Garcia <junior.opto@gmail.com>
2019-12-12 19:51:07 -0300

    Lesson 8 complete

---
+++ chapter_8/app.js
@@ -0,0 +1,43 @@
+const correctAnswers = ['B', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form')
+const scoreUpdate = document.querySelector('.result')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+   
+    let score = 0;
+    
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    //check answers
+    userAnswers.forEach((answer, index) => {
+        
+        if (answer === correctAnswers[index]){
+            score += 25;
+        }
+        index++;
+    });
+    
+     
+
+     scrollTo(0,0);
+    //  scoreUpdate.querySelector('span').textContent = `${score / correctAnswers.length * 100}%`;
+     scoreUpdate.classList.remove('d-none');
+
+    let output = 0;
+    const timer = setInterval(() => {
+        scoreUpdate.querySelector('span').textContent = `${output}%`;
+       if (output === score){
+            clearInterval(timer);
+       } else {
+            output++;
+       }
+    }, 10);
+
+
+     
+});
+
+
+    
+
