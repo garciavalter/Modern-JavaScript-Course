db46cd189c99aa56179bc7e768b0ea8b5fb482a6
Valter Garcia <junior.opto@gmail.com>
2019-12-14 15:48:28 -0300

    Lesson 84 done

--- chapter_11/sandbox.js
+++ chapter_11/sandbox.js
@@ -46,3 +46,28 @@ console.log(`this class was recorded ${days} ago`);
 
 const timestamp = 1675938474990;
 console.log(new Date(timestamp));
+
+// digital clock
+
+const clock = document.querySelector('.clock');
+
+const tick = () => { 
+
+  const now = new Date();
+  
+  const h = now.getHours();
+  const m = now.getMinutes();
+  const s = now.getSeconds();
+  
+  const html = `
+  <span>${h}</span> : 
+  <span>${m}</span> : 
+  <span>${s}</span>
+  `;
+  
+  clock.innerHTML = html;
+
+  
+};
+
+setInterval(tick, 1000);
