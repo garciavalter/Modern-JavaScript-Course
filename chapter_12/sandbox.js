afd4cbb21f8c26c11764639b0c8d8093b635fa44
Valter Garcia <junior.opto@gmail.com>
2019-12-15 21:29:28 -0300

    Lesson 91 done.

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,15 +1,34 @@
-const request = new XMLHttpRequest();
+const getTodos = (callback) => {
+    const request = new XMLHttpRequest();
 
-request.addEventListener('readystatechange', () => {
+    request.addEventListener('readystatechange', () => {
+
+        // console.log(request, request.readyState);
+
+        if(request.readyState === 4 && request.status === 200) {
+            callback(undefined, request.responseText);
+        } else if (request.readyState === 4){
+            callback('could not fetch data, error: ', undefined);
+        }
+
+    });
 
 
-    // console.log(request, request.readyState);
+request.open('GET', 'https://jsonplaceholder.typicode.com/todoss/');
+request.send();
+};
 
-    if(request.readyState === 4) {
-        console.log(request.responseText);
+console.log(1);
+console.log(2);
+
+getTodos((err, data) => {
+    console.log('callback fired!')
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
     }
-
 });
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/');
-request.send();
+console.log(3);
+console.log(4);
