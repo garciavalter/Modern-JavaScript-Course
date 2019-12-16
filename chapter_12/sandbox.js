4f017cd5785632ba0a21b1e50de772f0933ef241
Valter Garcia <junior.opto@gmail.com>
2019-12-15 23:59:58 -0300

    Lesson 98 done.

--- chapter_12/sandbox.js
+++ chapter_12/sandbox.js
@@ -1,10 +1,32 @@
-// fetch api
+// async & await
 
-fetch('todos/luigi.json').then((response) => {
-    console.log('resolved', response);
-    return response.json();
-}).then(data =>{
-    console.log(data);
-}).catch((err) => {
-    console.log('error');
-});
+const getTodos = async () => {
+
+    const response =  await fetch('todos/luigi.json');
+    const data = await response.json();
+    return data;
+
+};
+
+console.log(1);
+console.log(2);
+
+getTodos()
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
+    console.log(3);
+    console.log(4);
+
+// console.log(data);
+
+// // fetch api
+
+// fetch('todos/luigi.json').then((response) => {
+//     console.log('resolved', response);
+//     return response.json();
+// }).then(data =>{
+//     console.log(data);
+// }).catch((err) => {
+//     console.log('error');
+// });
