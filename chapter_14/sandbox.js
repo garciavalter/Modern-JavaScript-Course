06d02a23a72f5944d068540776911649e464fc08
Valter Garcia <junior.opto@gmail.com>
2019-12-17 10:40:29 -0300

    Lesson 112 done.

---
+++ chapter_14/sandbox.js
@@ -0,0 +1,32 @@
+// // store data in local storage
+// localStorage.setItem('name', 'mario');
+// localStorage.setItem('age', 50);
+
+
+// get data from local storage
+let name = localStorage.getItem('name');
+let age = localStorage.getItem('age');
+
+console.log(name, age);
+
+
+// updating data
+
+localStorage.setItem('name', 'luigi');
+localStorage.age = 40;
+
+name = localStorage.getItem('name');
+age = localStorage.getItem('age');
+
+console.log(name, age);
+
+
+// deleting data from local storage
+
+localStorage.removeItem('name');
+
+localStorage.clear();
+name = localStorage.getItem('name');
+age = localStorage.getItem('age')
+
+console.log(name, age);
