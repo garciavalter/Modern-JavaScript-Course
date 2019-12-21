872481da332a8d137843c291ddf6750889323864
Valter Garcia <junior.opto@gmail.com>
2019-12-20 21:00:55 -0300

    Buttons update for a better code maintance....delete messages problem solved, just need to refresh room history when select to gete 100%

--- chapter_17/app.js
+++ chapter_17/app.js
@@ -1,96 +1,76 @@
-const btnGeneral = document.querySelector('.btn-general');
-const btnGaming = document.querySelector('.btn-gamming');
-const btnMusic = document.querySelector('.btn-music');
-const btnNinjas = document.querySelector('.btn-ninjas');
+const channelSelector = document.querySelectorAll('.channel-selector');
+const roomHistory = document.querySelectorAll('.room-history')
 const messageToSent = document.querySelector('.message-to-sent');
 const newName = document.querySelector('.new-name');
 const btnSend = document.querySelector('.btn-send');
 const btnUpdateName = document.querySelector('.btn-update-name');
-const roomHistoryGeneral = document.querySelector('.room-history-general')
-const roomHistoryGamming = document.querySelector('.room-history-gamming')
-const roomHistoryMusic = document.querySelector('.room-history-music')
-const roomHistoryNinjas = document.querySelector('.room-history-ninjas')
-const chatRoomList = document.querySelector('.chatroom-list')
+const roomHistoryGeneral = document.querySelector('.room-history-general');
+const roomHistoryGamming = document.querySelector('.room-history-gamming');
+const roomHistoryMusic = document.querySelector('.room-history-music');
+const roomHistoryNinjas = document.querySelector('.room-history-ninjas');
+const chatRoomList = document.querySelector('.chatroom-list');
 
 const now = new Date();
 
-let userName = 'Anon'
+let userName = 'Anon';
 let currentChannel = 'General';
 
-btnGeneral.addEventListener('click', e =>{
-    e.preventDefault();
-    currentChannel='General';
-    roomHistoryGeneral.classList.remove("hide");
-    roomHistoryGamming.classList.add("hide");
-    roomHistoryMusic.classList.add("hide");
-    roomHistoryNinjas.classList.add("hide");
+channelSelector.forEach(button => {
+    button.addEventListener('click', e => {
+        e.preventDefault();
+        currentChannel = button.getAttribute('button-id');
+        roomHistory.forEach(room => {
+            room.getAttribute('room-id') == currentChannel ?
+                room.classList.remove("hide") : room.classList.add("hide");
+        })
     });
-
-btnGaming.addEventListener('click', e =>{
-    e.preventDefault();
-    currentChannel='Gamming';
-    roomHistoryGeneral.classList.add("hide");
-    roomHistoryGamming.classList.remove("hide");
-    roomHistoryMusic.classList.add("hide");
-    roomHistoryNinjas.classList.add("hide");
-});
-
-btnMusic.addEventListener('click', e =>{
-    e.preventDefault();
-    currentChannel='Music';
-    roomHistoryGeneral.classList.add("hide");
-    roomHistoryGamming.classList.add("hide");
-    roomHistoryMusic.classList.remove("hide");
-    roomHistoryNinjas.classList.add("hide");
-});
-btnNinjas.addEventListener('click', e =>{
-    e.preventDefault();
-    currentChannel='Ninjas';
-    roomHistoryGeneral.classList.add("hide");
-    roomHistoryGamming.classList.add("hide");
-    roomHistoryMusic.classList.add("hide");
-    roomHistoryNinjas.classList.remove("hide");
 });
 
 const sendMessage = (message, id) => {
     let time = message.timeStamp.toDate();
     let html = `
-    <li class="list-group-item" data-id"=${id}">
+    <li class="list-group-item" data-id="${id}">
     <div><strong>${message.userName}</strong>: ${message.message}</div>
     <div class="timestamp">${time}</div>
     </li>
     `
     switch(currentChannel){
-        case 'General':
+        case "General":
             roomHistoryGeneral.innerHTML += html;
+            console.log('Data added to: ' + currentChannel);
             break;
 
         case 'Gamming':
             roomHistoryGamming.innerHTML += html;
+            console.log('Data added to: ' + currentChannel);
             break;
     
         case 'Music':
             roomHistoryMusic.innerHTML += html;
+            console.log('Data added to: ' + currentChannel);
             break;
     
         case 'Ninjas':
             roomHistoryNinjas.innerHTML += html;
+            console.log('Data added to: ' + currentChannel);
     }
     
 }
 const deleteMessage = (id) => {
     const messages = document.querySelectorAll('li');
-    console.log(messages);
     messages.forEach(message => {
-        if(messages.getAttribute('data-id') === id){
-            messages.remove();
+        if(message.getAttribute('data-id') === id){
+            message.remove();
         }
     });
 };
 
+
 db.collection(currentChannel).onSnapshot(snapshot => {
     snapshot.docChanges().forEach(change => {
+        
         const doc = change.doc;
+        console.log(doc);
         if(change.type ==='added'){
             sendMessage(doc.data(), doc.id);
         } else if (change.type === 'removed'){
@@ -99,6 +79,7 @@ db.collection(currentChannel).onSnapshot(snapshot => {
         
     })
 })
+
 
 btnSend.addEventListener('click', e => {
     e.preventDefault();
