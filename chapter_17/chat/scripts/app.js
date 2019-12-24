92dd6f1b280d998e829a43fd6b09c2e42efbc842
Valter Garcia <junior.opto@gmail.com>
2019-12-24 14:46:56 -0300

    send message function done.

--- chapter_17/chat/scripts/app.js
+++ chapter_17/chat/scripts/app.js
@@ -1,6 +1,17 @@
 // DOM queries
 
 const chatList = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat')
+
+//add a new chat
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+    .then(() => newChatForm.reset())
+    .catch(err => console.log(err));
+})
 
 // class instances
 const chatUI = new ChatUI(chatList);
@@ -8,3 +19,4 @@ const chatroom = new Chatroom('general', 'shaun');
 
 // get chats render
 chatroom.getChats(data =>     chatUI.render(data));
+
--- chapter_17/chat/scripts/ui.js
+++ chapter_17/chat/scripts/ui.js
@@ -3,11 +3,15 @@ class ChatUI {
         this.list = list;
     }
     render(data){
+        const when = dateFns.distanceInWordsToNow(
+            data.created_at.toDate(),
+            {addSuffix: true}
+        );
         const html =`
         <li class="list-group-item">
             <span class="username">${data.username}</span>
             <span class="message">${data.message}</span>
-            <div class="time">${data.created_at.toDate()}</div>
+            <div class="time">${when}</div>
         
         </li>
         `;
