7aae076b33472a7238bfc0fb36134a10b6cb5d87
Valter Garcia <junior.opto@gmail.com>
2019-12-25 11:19:50 -0300

    Lesson 149 done.

--- chapter_18/sandbox.js
+++ chapter_18/sandbox.js
@@ -28,3 +28,42 @@ const personClone = {...person, location: 'manchester'};
 
 console.log(personClone);
 
+// sets 
+
+const namesArray = ['ryu', 'chun-li', 'shaun', 'ryu', 'chun-li'];
+console.log(namesArray);
+
+// const namesSet = new Set(['ryu', 'chun-li', 'shaun', 'ryu', 'chun-li']);
+// const namesSet = new Set(namesArray);
+// console.log(namesSet);
+
+// const uniqueNames = [...namesSet]
+// console.log(uniqueNames);
+
+const uniqueNames = [...new Set(namesArray)];
+console.log(uniqueNames);
+
+const ages = new Set();
+
+ages.add(20);
+ages.add(25).add(30);
+ages.add(25);
+ages.delete(25);
+
+console.log(ages, ages.size);
+
+console.log(ages.has(30), ages.has(25));
+
+ages.clear();
+console.log(ages);
+
+const ninjas = new Set([
+    {name: 'shaun', age: 30},
+    {name: 'crystal', age: 29},
+    {name: 'chun-li', age: 32},
+]);
+
+ninjas.forEach(ninja => {
+    console.log(ninja.name, ninja.age);
+})
+
