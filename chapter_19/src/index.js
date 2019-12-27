2b942cbae1b67f05a35589952f5e1657e9767b4b
Valter Garcia <junior.opto@gmail.com>
2019-12-27 18:49:06 -0300

    Webpack setup to live listner.

--- chapter_19/src/index.js
+++ chapter_19/src/index.js
@@ -2,5 +2,6 @@ import {styleBody, addTitle, contact} from './dom';
 import users, { getPremUsers } from './data';
 
 const premUsers = getPremUsers(users);
-
 console.log(users, premUsers);
+
+console.log('test');
