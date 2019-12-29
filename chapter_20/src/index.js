ee678470749490dc824544122320118687b30490
Valter Garcia <junior.opto@gmail.com>
2019-12-29 12:17:02 -0300

    Dropdowns done.

--- chapter_20/src/index.js
+++ chapter_20/src/index.js
@@ -1,7 +1,18 @@
 import Tooltip from './ninja-ui/tooltip';
+import Dropdown from './ninja-ui/dropdown';
 
 // create a tooltip
 
 const tooltip = new Tooltip(document.querySelector('.tooltip'));
-
 tooltip.init();
+
+// create dropdowns
+
+const dropdowns = document.querySelectorAll('.dropdown');
+console.log(dropdowns);
+
+dropdowns.forEach( dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+    console.log('instance initialized');
+});
